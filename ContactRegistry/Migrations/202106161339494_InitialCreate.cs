48054ea0daabfa8fa7b9433c64da5b368eff399b
Elin Strandberg <elin-strandberg@hotmail.com>
2021-06-16 21:33:34 +0200

    Migration files

---
+++ ContactRegistry/Migrations/202106161339494_InitialCreate.cs
@@ -0,0 +1,58 @@
+﻿namespace ContactRegistry.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Company",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        CompanyName = c.String(nullable: false),
+                        Address = c.String(),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+            CreateTable(
+                "dbo.Enrollment",
+                c => new
+                    {
+                        EnrollmentID = c.Int(nullable: false, identity: true),
+                        ContactID = c.Int(nullable: false),
+                        CompanyID = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.EnrollmentID)
+                .ForeignKey("dbo.Company", t => t.CompanyID, cascadeDelete: true)
+                .ForeignKey("dbo.Contact", t => t.ContactID, cascadeDelete: true)
+                .Index(t => t.ContactID)
+                .Index(t => t.CompanyID);
+            
+            CreateTable(
+                "dbo.Contact",
+                c => new
+                    {
+                        ContactID = c.Int(nullable: false, identity: true),
+                        ContactName = c.String(nullable: false),
+                        Title = c.String(),
+                        PhoneNumber = c.String(),
+                    })
+                .PrimaryKey(t => t.ContactID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Enrollment", "ContactID", "dbo.Contact");
+            DropForeignKey("dbo.Enrollment", "CompanyID", "dbo.Company");
+            DropIndex("dbo.Enrollment", new[] { "CompanyID" });
+            DropIndex("dbo.Enrollment", new[] { "ContactID" });
+            DropTable("dbo.Contact");
+            DropTable("dbo.Enrollment");
+            DropTable("dbo.Company");
+        }
+    }
+}
---
+++ ContactRegistry/Migrations/Configuration.cs
@@ -0,0 +1,79 @@
+﻿namespace ContactRegistry.Migrations
+{
+    using ContactRegistry.Models;
+    using System;
+    using System.Collections.Generic;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<ContactRegistry.DAL.RegistryContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(ContactRegistry.DAL.RegistryContext context)
+        {
+            var companies = new List<Company>
+            {
+                new Company{ CompanyName="Företaget", Address="Vägen 123" },
+                new Company{ CompanyName="MyCode", Address="Gatan 123"},
+                new Company{ CompanyName="Future Ltd", Address="Stigen 123"}
+
+            };
+            companies.ForEach(c => context.Companies.AddOrUpdate(p => p.CompanyName, c));
+            context.SaveChanges();
+
+            var contacts = new List<Contact>
+            {
+                new Contact{ContactID = 1050, ContactName="Elin",Title="Aspiring developer",PhoneNumber="070-123345" },
+                new Contact{ContactID = 1051, ContactName="Kalle",Title="Manager",PhoneNumber="070-14567"},
+                new Contact{ContactID = 1052, ContactName="Igor",Title="Kaffeansvarig",PhoneNumber="070-123678"},
+                new Contact{ContactID = 1053, ContactName="Lovisa",Title="Praktikant",PhoneNumber="070-123678" }
+
+            };
+            contacts.ForEach(c => context.Contacts.AddOrUpdate(p => p.ContactName, c));
+            context.SaveChanges();
+
+            var enrollments = new List<Enrollment>
+            {
+                new Enrollment{
+                    CompanyID = companies.Single(d => d.CompanyName == "Företaget").ID,
+                    ContactID = contacts.Single(s => s.ContactName == "Elin").ContactID
+                },
+                new Enrollment{
+                    CompanyID = companies.Single(d => d.CompanyName == "Företaget").ID,
+                    ContactID = contacts.Single(s => s.ContactName == "Kalle").ContactID
+                },
+                new Enrollment{
+                    CompanyID = companies.Single(d => d.CompanyName == "MyCode").ID,
+                    ContactID = contacts.Single(s => s.ContactName == "Igor").ContactID
+                },
+                new Enrollment{
+                    CompanyID = companies.Single(d => d.CompanyName == "MyCode").ID,
+                    ContactID = contacts.Single(s => s.ContactName == "Lovisa").ContactID
+                },
+                new Enrollment{
+                    CompanyID = companies.Single(d => d.CompanyName == "Future Ltd").ID,
+                    ContactID = contacts.Single(s => s.ContactName == "Elin").ContactID
+                }
+
+            };
+
+            foreach (Enrollment e in enrollments)
+            {
+                var enrollmentInDataBase = context.Enrollments.Where(
+                    s =>
+                         s.Company.ID == e.CompanyID &&
+                         s.Contact.ContactID == e.ContactID).SingleOrDefault();
+                if (enrollmentInDataBase == null)
+                {
+                    context.Enrollments.Add(e);
+                }
+            }
+            context.SaveChanges();
+        }
+    }
+}
